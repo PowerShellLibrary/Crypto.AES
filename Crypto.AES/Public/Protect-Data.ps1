ebd97ef0622734d373a61239ac47417af3569f20
Alan Płócieniak <alan-null@outlook.com>
2025-03-16 18:10:50 +0100

    #2 | Disposing of the AesGcm instance unconditionally
    fixes #2

--- Crypto.AES/Public/Protect-Data.ps1
+++ Crypto.AES/Public/Protect-Data.ps1
@@ -1,15 +1,19 @@
 function Protect-Data {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "Key" )]
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "Key")]
         [byte[]]$Key,
-        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "GCM" )]
+
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "GCM")]
         [System.Security.Cryptography.AesGcm]$GCM,
-        [Parameter(Mandatory = $true, Position = 1 )]
+
+        [Parameter(Mandatory = $true, Position = 1)]
         [byte[]]$Data,
-        [Parameter(Mandatory = $false, Position = 2 )]
+
+        [Parameter(Mandatory = $false, Position = 2)]
         [byte[]]$Nonce,
-        [Parameter(Mandatory = $false, Position = 3 )]
+
+        [Parameter(Mandatory = $false, Position = 3)]
         [Switch]$Combined
     )
 
@@ -27,17 +31,23 @@ function Protect-Data {
         $tag = [byte[]]::new(16)
 
         if ($PSCmdlet.ParameterSetName -eq 'Key') {
-            $gcm = [System.Security.Cryptography.AesGcm]::new($Key)
+            $GCM = [System.Security.Cryptography.AesGcm]::new($Key)
+            try {
+                $GCM.Encrypt($Nonce, $Data, $cipherOutput, $tag)
+            }
+            finally {
+                $GCM.Dispose()
+            }
         }
-
-        $gcm.Encrypt($Nonce, $Data, $cipherOutput, $tag)
-        $gcm.Dispose()
+        else {
+            $GCM.Encrypt($Nonce, $Data, $cipherOutput, $tag)
+        }
 
         if ($Combined) {
             $output = [byte[]]::new($cipherOutput.Length + $Nonce.Length + $tag.Length)
-            [System.Buffer]::BlockCopy($tag, 0, $output, 0, $tag.Length);
-            [System.Buffer]::BlockCopy($cipherOutput, 0, $output, $tag.Length, $cipherOutput.Length);
-            [System.Buffer]::BlockCopy($Nonce, 0, $output, $tag.Length + $cipherOutput.Length, $Nonce.Length);
+            [System.Buffer]::BlockCopy($tag, 0, $output, 0, $tag.Length)
+            [System.Buffer]::BlockCopy($cipherOutput, 0, $output, $tag.Length, $cipherOutput.Length)
+            [System.Buffer]::BlockCopy($Nonce, 0, $output, $tag.Length + $cipherOutput.Length, $Nonce.Length)
             Write-Output $output -NoEnumerate
         }
         else {
